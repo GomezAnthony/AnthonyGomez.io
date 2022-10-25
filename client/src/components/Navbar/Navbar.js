ea5b1541b71f55665c864a53d17755a9f1440a5b
Anthony Gomez <gomez9162@gmail.com>
2022-10-25 17:14:31 -0500

    Navbar
    Add navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='navbar'>
+            <div className='navbar__container'>
+                <div className='navbar__logo'></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
